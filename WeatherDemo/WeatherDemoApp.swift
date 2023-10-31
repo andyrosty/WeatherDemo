98c408fb03dfd14de01a62093b09db0f4a46a943
Andrew Acheampong <gorgi.andy@gmail.com>
2023-10-31 15:50:03 +0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Andrew Acheampong on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
